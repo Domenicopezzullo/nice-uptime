da658c5999bd7284bf757d5eef82262b852a65b9
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-07-06 17:53:15 +0200

    Yes

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	_ "modernc.org/sqlite"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "skibidi.db")
+	db, err := sql.Open("sqlite", "skibidi.db")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -18,10 +20,43 @@ func main() {
 	s := gin.Default()
 	s.LoadHTMLGlob("templates/*")
 	s.GET("/", func(ctx *gin.Context) {
-		_, err = db.Exec("SELECT * from users")
+		ctx.HTML(200, "login.html", gin.H{})
+	})
+	s.POST("/", func(ctx *gin.Context) {
+		username := ctx.PostForm("username")
+		_ = ctx.PostForm("password")
+
+		fmt.Println(username)
+
+		rows := db.QueryRow("SELECT * FROM users where username = '?'", username)
+		fmt.Printf("SELECT * FROM users where username = '%s'\n", username)
 		if err != nil {
 			panic(err.Error())
 		}
-		ctx.HTML(200, "login.html", gin.H{})
+
+		fmt.Println(rows)
+
+		err := rows.Scan()
+		if err != nil && err == sql.ErrNoRows {
+			ctx.JSON(404, gin.H{
+				"authorized": false,
+				"message":    "I cannot let you in, stranger",
+			})
+			return
+		} else if err != nil {
+			ctx.JSON(500, gin.H{
+				"authorized": false,
+				"message":    "A problem occurred",
+			})
+			return
+		}
+
+		ctx.JSON(200, gin.H{
+			"authorized": true,
+			"message":    fmt.Sprintf("Welcome, %s", username),
+		})
+
 	})
+
+	s.Run(":8080")
 }
